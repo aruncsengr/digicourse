8d42bc0c14ded78a517357ce5b12d330e5f121f3
Arun Kumar <aruncsengr@gmail.com>
2024-06-18 03:13:48 +0530

    chore: removed unneccessary files

--- app/serializers/v1/course_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
-
-  has_many :tutors
-end
--- app/serializers/v1/tutor_serializer.rb
+++
@@ -1,5 +0,0 @@
-class TutorSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
-
-  belongs_to :course
-end
